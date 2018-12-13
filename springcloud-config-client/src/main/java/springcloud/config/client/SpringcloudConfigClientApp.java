09b9b76d0e8bad348debf9f9af5e23e3d685bf70
xiongchao <xiongchao@zafh.com.cn>
2018-12-13 16:18:30 +0800

    spring cloud-client config;

--- springcloud-config-client/src/main/java/springcloud/config/client/SpringcloudConfigClientApp.java
+++ springcloud-config-client/src/main/java/springcloud/config/client/SpringcloudConfigClientApp.java
@@ -1,16 +1,16 @@
-package springcloud.provider;
+package springcloud.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableConfigServer
-public class SpringcloudProviderApp {
+public class SpringcloudConfigClientApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudProviderApp.class, args);
+        SpringApplication.run(SpringcloudConfigClientApp.class, args);
     }
+
 }
+
---
+++ springcloud-config-client/src/main/java/springcloud/config/client/controller/ConfigClientController.java
@@ -0,0 +1,17 @@
+package springcloud.config.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ConfigClientController {
+
+    @Value("${yoong.profile}")
+    private String config;
+
+    @RequestMapping("configclient/getconfig")
+    public void getConfig() {
+        System.out.println(config);
+    }
+}
