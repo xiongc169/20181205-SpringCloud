19087118be745a5780df05d1b8b84587e4dfe741
xiongchao <xiongchao@zafh.com.cn>
2018-12-13 19:51:28 +0800

    spring cloud-server config;

--- springcloud-config-client/src/main/java/springcloud/config/client/controller/ConfigClientController.java
+++ springcloud-config-client/src/main/java/springcloud/config/client/controller/ConfigClientController.java
@@ -1,17 +1,27 @@
 package springcloud.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RefreshScope //使用@RefreshScope注解，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
 public class ConfigClientController {
 
     @Value("${yoong.profile}")
-    private String config;
+    private String dev;
 
+    //@Value("${profile}")
+    //private String test;
+
+    @ResponseBody
     @RequestMapping("config/getConfig")
-    public void getConfig() {
-        System.out.println(config);
+    public String getConfig() {
+        System.out.println(dev);
+        //System.out.println(test);
+        return dev;
+
     }
 }
