a94574c32f3f605f45fd4e1eefac41576f225a34
xiongchao <xiongchao@zafh.com.cn>
2018-12-18 18:53:35 +0800

    spring cloud turbine;

--- springcloud-turbine/src/main/java/springcloud/turbine/SpringcloudTurbineApp.java
+++ springcloud-turbine/src/main/java/springcloud/turbine/SpringcloudTurbineApp.java
@@ -2,9 +2,11 @@ package springcloud.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableHystrixDashboard
 @EnableTurbine
 public class SpringcloudTurbineApp {
 
